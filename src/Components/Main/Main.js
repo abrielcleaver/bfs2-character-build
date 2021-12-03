8e5da3154eb78e1a2179360a2943f9331da223b8
Abriel Cleaver <abrielcleaver@gmail.com>
2021-12-02 18:27:21 -0700

    files and folders created with export/import

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,14 @@
+import './Main.css';
+import React from 'react';
+
+import Editor from '../Editor/Editor';
+import Display from '../Display/Display';
+
+export default function Main() {
+  return (
+    <main>
+      <Display />
+      <Editor />
+    </main>
+  );
+}
